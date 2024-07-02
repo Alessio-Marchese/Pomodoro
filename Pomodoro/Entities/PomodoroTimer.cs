445093dcc8c1cb14e99110f01f20fbb7cc58ce8a
Alessio <alessio.marchese148@gmail.com>
2024-07-02 19:42:54 +0200

    Creato scheletro per la pagina settings, + componente per gestire la modifica dei timer

---
+++ Pomodoro/Entities/PomodoroTimer.cs
@@ -0,0 +1,13 @@
+﻿namespace Pomodoro.Entities;
+
+public class PomodoroTimer
+{
+    public string Name { get; set; } = string.Empty;
+    public int Time { get; set; }
+
+    public PomodoroTimer(string name, int time)
+    {
+        Name = name;
+        Time = time;
+    }
+}
