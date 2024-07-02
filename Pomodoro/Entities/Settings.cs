7e2922e344f5df9b36c50ff26ab1adf76ed70b5d
Alessio <alessio.marchese148@gmail.com>
2024-07-02 10:13:35 +0200

    Aggiunto classe e metodi per passare le varie tempistiche al timer

---
+++ Pomodoro/Entities/Settings.cs
@@ -0,0 +1,8 @@
+﻿namespace Pomodoro.Entities;
+
+public class Settings
+{
+    public static int Production { get; set; } = 25;
+    public static int ShortPause { get; set; } = 5;
+    public static int LongPause { get; set; } = 15;
+}
--- Pomodoro/MauiProgram.cs
+++ Pomodoro/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Pomodoro.Entities;
 
 namespace Pomodoro
 {
