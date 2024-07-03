20ebf1b18f5fc1530a826f0c19d00c06f6bea70b
Alessio <alessio.marchese148@gmail.com>
2024-07-03 12:15:32 +0200

    Modificato la struttura del progetto per gestire il timer in Home da una classe

---
+++ Pomodoro/Entities/PomodoroSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Pomodoro.Entities;
+
+public class PomodoroSettings
+{
+    public string Name { get; set; } = string.Empty;
+    public int Time { get; set; }
+
+    public PomodoroSettings(string name, int time)
+    {
+        Name = name;
+        Time = time;
+    }
+}
--- Pomodoro/Entities/PomodoroTimer.cs
+++ Pomodoro/Entities/PomodoroTimer.cs
@@ -1,13 +1,63 @@
-﻿namespace Pomodoro.Entities;
+﻿using System;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace Pomodoro.Entities;
 
 public class PomodoroTimer
 {
-    public string Name { get; set; } = string.Empty;
-    public int Time { get; set; }
+    public TimeSpan Time { get; set; }
+    public string FormattedTime { get; set; } = string.Empty;
+    public Timer Timer { get; set; }
 
-    public PomodoroTimer(string name, int time)
+    public event Action OnTimeChanged;
+    public PomodoroTimer()
     {
-        Name = name;
-        Time = time;
+        Timer = new(100);
+        Timer.Elapsed += ReduceMilliseconds;
+        Timer.AutoReset = true;
+        Timer.Enabled = true;
+    }
+
+    private void ReduceMilliseconds(Object source, ElapsedEventArgs e)
+    {   if(Time.TotalMilliseconds == 0)
+        {
+            Timer.Stop();
+            return;
+        }
+        Time = Time.Subtract(TimeSpan.FromMilliseconds(100));
+        FormattedTime = Time.ToString(@"mm\:ss");
+        OnTimeChanged?.Invoke();
+    }
+
+    public void Break()
+    {
+        Timer.Stop();
+    }
+
+    public void Start()
+    {
+        Timer.Start();
+    }
+
+    public void SetProduction()
+    {
+        Time = new TimeSpan(0, 0, Preferences.Get("Production", 25), 0, 100);
+        FormattedTime = Time.ToString(@"mm\:ss");
+        Break();
+    }
+
+    public void SetShortPause()
+    {
+        Time = new TimeSpan(0, 0, Preferences.Get("ShortPause", 5), 0, 100);
+        FormattedTime = Time.ToString(@"mm\:ss");
+        Break();
+    }
+
+    public void SetLongPause()
+    {
+        Time = new TimeSpan(0, 0, Preferences.Get("LongPause", 10), 0, 100);
+        FormattedTime = Time.ToString(@"mm\:ss");
+        Break();
     }
 }
