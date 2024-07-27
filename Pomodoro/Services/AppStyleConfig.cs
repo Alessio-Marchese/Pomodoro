406f88a0dc2184907e67e3ee3d91cbe72b62d64d
Alessio <alessio.marchese148@gmail.com>
2024-07-27 20:25:04 +0200

    aggiunto tema chiaro scuro

---
+++ Pomodoro/Services/AppStyleConfig.cs
@@ -0,0 +1,32 @@
+﻿namespace Pomodoro.Services;
+
+public class AppStyleConfig
+{
+    public static string CurrentThemeBackground = Preferences.Get("CurrentThemeBackground", LightTheme);
+    public static string CurrentThemeItems = Preferences.Get("CurrentThemeItems", DarkTheme);
+    public static string CurrentBootstrapButton = Preferences.Get("CurrentBootstrapButton", DarkBootstrapButton);
+    private const string LightBootstrapButton = "light";
+    private const string DarkBootstrapButton = "dark";
+    private const string DarkTheme = "#404040";
+    private const string LightTheme = "white";
+
+    public static void SetDarkTheme()
+    {
+        CurrentThemeBackground = DarkTheme;
+        CurrentThemeItems = LightTheme;
+        CurrentBootstrapButton = LightBootstrapButton;
+        Preferences.Set("CurrentThemeBackground", CurrentThemeBackground);
+        Preferences.Set("CurrentThemeItems", CurrentThemeItems);
+        Preferences.Set("CurrentBootstrapButton", CurrentBootstrapButton);
+    }
+
+    public static void SetLightTheme()
+    {
+        CurrentThemeBackground = LightTheme;
+        CurrentThemeItems = DarkTheme;
+        CurrentBootstrapButton = DarkBootstrapButton;
+        Preferences.Set("CurrentThemeBackground", CurrentThemeBackground);
+        Preferences.Set("CurrentThemeItems", CurrentThemeItems);
+        Preferences.Set("CurrentBootstrapButton", CurrentBootstrapButton);
+    }
+}
--- Pomodoro/Services/NotifyChange.cs
+++ Pomodoro/Services/NotifyChange.cs
@@ -4,6 +4,10 @@ public class NotifyChange
 {
     public static event Action OnChange;
     public static event Action OnTimerComplete;
+    public static event Action NavBarChange;
     public static void TimerCompleted() => OnTimerComplete?.Invoke();
     public static void HomeRefresh() => OnChange?.Invoke();
+    public static void NavBarRefresh() => NavBarChange?.Invoke();
+
+    
 }
